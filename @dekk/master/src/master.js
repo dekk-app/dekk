1a978665c30c16f5f9ceef040199939218147e80
Gregor Adams <greg@pixelass.com>
2018-02-02 12:52:08 +0100

    feat(master): slide fragemnts.
    Add a mechanism to define fragments.
    
    Signed-off-by: Gregor Adams <greg@pixelass.com>

--- @dekk/master/src/master.js
+++ @dekk/master/src/master.js
@@ -9,82 +9,112 @@ class Master extends Component {
   render() {
     const {children, content, pageIndex, current} = this.props
     // All `Slot` instances
-    const slots = Children.toArray(children)
-      .filter(child => child.type === Slot)
+    const slots = Children.toArray(children).filter(
+      child => child.type === Slot
+    )
 
     // All `Static` instances
-    const statics = Children.toArray(children)
-      .filter(child => child.type === Static)
+    const statics = Children.toArray(children).filter(
+      child => child.type === Static
+    )
 
     // Helper slots are filtered from the children.
     const helperSlots = [Notes]
 
     // Filtered children of the component.
     // Excludes helperSlots
-    const onlyContent = Children.toArray(content)
-      .filter(child => !(helperSlots.includes(child.type)))
+    const onlyContent = Children.toArray(content).filter(
+      child => !helperSlots.includes(child.type)
+    )
 
     const filledStatics = statics.map((item, i) => (
-      <div key={`static__${i}`}
-           data-static={item.props.name}>
+      <div key={`static__${i}`} data-static={item.props.name}>
         {item.props.children}
       </div>
     ))
 
-    const filledSlots = slots.map((item, i) => {
-      const {only, not, required, component, name} = item.props
-      const index = onlyContent.map(child => child.type).indexOf(component)
-      if (index < 0) {
-        if (required) {
-          return (
-            <div key={`slot__${i}`} data-slot={name}>
-              <Warning {...item.props} missing/>
-            </div>
-          )
+    // Allow page fragments
+    const fragments = onlyContent
+      .reduce((a, b) => a.concat(b), [])
+      .map(x => x.props.children)
+      .reduce((a, b) => a.concat(b), [])
+      .filter(x => typeof x === 'object' && typeof x.props.fragment === 'number')
+      .sort((a, b) => {
+        if (a.props.fragment > b.props.fragment) {
+          return 1
+        } else if (a.props.fragment < b.props.fragment) {
+          return -1
         }
-        return null
-      }
-      const children = Children.toArray(onlyContent[index].props.children)
-        .map((child, idx) => {
+        return 0
+      })
+
+    const filledSlots = slots
+      .map((item, i) => {
+        const {only, not, required, component, name} = item.props
+        const index = onlyContent.map(child => child.type).indexOf(component)
+
+        if (index < 0) {
+          if (required) {
+            return (
+              <div key={`slot__${i}`} data-slot={name}>
+                <Warning {...item.props} missing />
+              </div>
+            )
+          }
+          return null
+        }
+        const children = Children.toArray(
+          onlyContent[index].props.children
+        ).map((child, idx) => {
           if (only) {
             if (only.includes(child.type)) {
               return child
             }
             return (
-              <Warning {...item.props}
-                       key={`slot_${idx}`}
-                       type={(child.type && (child.type.displayName || child.type.name)) || `"${child}"`}
-                       invalid/>
+              <Warning
+                {...item.props}
+                key={`slot_${idx}`}
+                type={
+                  (child.type && (child.type.displayName || child.type.name)) ||
+                  `"${child}"`
+                }
+                invalid
+              />
             )
           }
           if (not) {
             if (not.includes(child.type)) {
               return (
-                <Warning {...item.props}
-                         key={`slot_${idx}`}
-                         type={(child.type && (child.type.displayName || child.type.name)) || `"${child}"`}
-                         invalid/>
+                <Warning
+                  {...item.props}
+                  key={`slot_${idx}`}
+                  type={
+                    (child.type &&
+                      (child.type.displayName || child.type.name)) ||
+                    `"${child}"`
+                  }
+                  invalid
+                />
               )
             }
             return child
           }
           return child
         })
-      if (required && children.length === 0) {
+        if (required && children.length === 0) {
+          return (
+            <div key={`item__${i}`} data-slot={item.props.name}>
+              <Warning {...item.props} missing />
+            </div>
+          )
+        }
         return (
           <div key={`item__${i}`} data-slot={item.props.name}>
-            <Warning {...item.props} missing/>
+            {children}
           </div>
         )
-      }
-      return (
-        <div key={`item__${i}`}
-             data-slot={item.props.name}>
-          {children}
-        </div>
-      )
-    }).filter(x => Boolean(x))
-
+      })
+      .filter(x => Boolean(x))
 
     const title = null
 
@@ -100,16 +130,16 @@ class Master extends Component {
 
 Master.propTypes = {
   children: (props, propName, componentName) => {
-      let error;
-      const prop = props[propName];
-      Children.forEach(prop, (child) => {
-        if ([Static, Slot].indexOf(child.type) < 0) {
-          error = new Error(
-            `"Master" only accepts children of type "Slot" or "Static"`
-          )
-        }
-      })
-      return error;
+    let error
+    const prop = props[propName]
+    Children.forEach(prop, child => {
+      if ([Static, Slot].indexOf(child.type) < 0) {
+        error = new Error(
+          `"Master" only accepts children of type "Slot" or "Static"`
+        )
+      }
+    })
+    return error
   },
   content: PropTypes.node,
   pageIndex: PropTypes.number,
