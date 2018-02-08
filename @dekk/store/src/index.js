f6b8fd46ac79783155a884d2a15add5311072767
Gregor Adams <greg@pixelass.com>
2018-02-08 10:48:56 +0100

    feat(store): Better bragment handling
    Use fragmentHosts to find fragments
    
    Signed-off-by: Gregor Adams <greg@pixelass.com>

--- @dekk/store/src/index.js
+++ @dekk/store/src/index.js
@@ -1,14 +1,15 @@
 import uuid from 'uuid/v4'
-import { observable } from 'mobx'
+import {observable} from 'mobx'
 
 export default class Store {
   id = uuid()
   @observable page = 0
   @observable fragment = 0
-  @observable fragments = 0
-  @observable direction  = 0
+  @observable fragmentCount = 0
+  @observable direction = 0
   constructor(props) {
     this.page = props.page
+    this.fragmentHosts = props.fragmentHosts
     this.title = props.title
 
     this.setTitle = this.setTitle.bind(this)
@@ -16,7 +17,6 @@ export default class Store {
     this.goToPage = this.goToPage.bind(this)
     this.nextPage = this.nextPage.bind(this)
     this.previousPage = this.previousPage.bind(this)
-    this.setFragments = this.setFragments.bind(this)
     this.goToFragment = this.goToFragment.bind(this)
     this.nextFragment = this.nextFragment.bind(this)
     this.previousFragment = this.previousFragment.bind(this)
@@ -43,19 +43,17 @@ export default class Store {
     this.goToPage(this.page - 1)
   }
 
-  setFragments(n) {
-    this.fragments = n
-  }
-
   goToFragment(n) {
-    this.fragment = n
+    const m = this.fragmentHosts[this.page][n]
+    this.fragment = m
+    this.fragmentCount = n
   }
 
   nextFragment() {
-    this.goToFragment(this.fragment + 1)
+    this.goToFragment(this.fragmentCount + 1)
   }
 
   previousFragment() {
-    this.goToFragment(this.fragment - 1)
+    this.goToFragment(this.fragmentCount - 1)
   }
 }
