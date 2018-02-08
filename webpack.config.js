6671e01527eb7dfac73b3956b7415d08a19f8a02
Gregor Adams <greg@pixelass.com>
2018-02-08 10:59:23 +0100

    build(gh-pages): Add gh-pages build helpers
    Builds the required files for gh-pages (dev & prod)
    
    Signed-off-by: Gregor Adams <greg@pixelass.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin')
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 const ROOT = __dirname
 const DOCS = path.resolve(ROOT, 'docs')
@@ -27,5 +30,45 @@ module.exports = {
     compress: false,
     historyApiFallback: true,
     hot: false
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Dekk Demo',
+      filename: 'index.html',
+      template: 'gh-pages/index.html',
+      alwaysWriteToDisk: true,
+      minify: {
+        collapseWhitespace: true,
+        html5: true,
+        minifyCSS: true,
+        quoteCharacter: "\"",
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        sortClassName: true,
+        sortAttributes: true,
+        useShortDoctype: true
+
+      }
+    }),
+    new HtmlWebpackHarddiskPlugin({
+      outputPath: DOCS
+    }),
+    new FaviconsWebpackPlugin({
+      logo: './gh-pages/favico.png',
+      icons: {
+        android: true,
+        appleIcon: true,
+        appleStartup: false,
+        coast: false,
+        favicons: true,
+        firefox: false,
+        opengraph: false,
+        twitter: false,
+        yandex: false,
+        windows: false
+      }
+    })
+  ]
 }
