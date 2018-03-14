4cabe1a8845a3967f55bcc6bcafb6b93c8a58be2
Gregor Adams <greg@pixelass.com>
2018-03-14 21:57:48 +0100

    chore: patternplate icon size

--- patternplate.config.js
+++ patternplate.config.js
@@ -4,7 +4,7 @@ module.exports = {
   render: "@patternplate/render-styled-components/render",
   mount: "@patternplate/render-styled-components/mount",
   logo: `
-  <svg height="48" width="48" viewBox="0 0 24 24">
+  <svg height="60" width="100%" viewBox="0 0 24 24">
     <rect width="24" height="24" fill="hsl(212, 44%, 29%)"></rect>
     <polygon fill="hsl(50, 70%, 70%)" points="5,5.07 13,5.07 17,12 13,18.92 5,18.92"></polygon>
     <polygon fill="hsl(50, 10%, 90%)" stroke="hsl(212, 44%, 29%)" stroke-width="1" points="9,6.8 15,6.8 18,12 15,17.19 9,17.19"></polygon>
