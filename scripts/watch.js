6434aaad32acae9cc6ff97a98ce94243f177cabe
Gregor Adams <greg@pixelass.com>
2018-03-14 21:38:11 +0100

    chore: adjust build chain

--- scripts/watch.js
+++ scripts/watch.js
@@ -1,4 +1,4 @@
 const execa = require('execa')
 
 execa('yarn', ['run', 'babel:packages:watch'], {stdio: 'inherit'}).catch(console.error)
-execa('yarn', ['run', 'patternplate'], {stdio: 'inherit'}).catch(console.error)
+execa('yarn', ['run', 'watch'], {stdio: 'inherit'}).catch(console.error)
