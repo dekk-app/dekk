0f896bb9b374732dadfb936119b13e472957e11c
Gregor Adams <greg@pixelass.com>
2018-03-21 02:23:39 +0100

    chore: adjust patternplate

--- patternplate.config.js
+++ patternplate.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  docs: ["manual/**/*.md"],
-  entry: ["@dekk/**/lib/**/demo.js"],
-  render: "@patternplate/render-styled-components/render",
-  mount: "@patternplate/render-styled-components/mount",
+  docs: ['manual/**/*.md'],
+  entry: ['@dekk/**/lib/**/demo.js'],
+  render: '@patternplate/render-react/render',
+  mount: '@patternplate/render-styled-components/mount',
   logo: `
   <svg height="60" width="100%" viewBox="0 0 24 24">
     <rect width="24" height="24" fill="hsl(212, 44%, 29%)"></rect>
@@ -10,6 +10,6 @@ module.exports = {
     <polygon fill="hsl(50, 10%, 90%)" stroke="hsl(212, 44%, 29%)" stroke-width="1" points="9,6.8 15,6.8 18,12 15,17.19 9,17.19"></polygon>
   </svg>`,
   ui: {
-    title: "Dekk"
+    title: 'Dekk'
   }
-};
+}
